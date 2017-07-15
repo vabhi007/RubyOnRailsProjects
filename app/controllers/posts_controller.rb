b34ce6ff784ee5c3f75f9ad09340f3535985a0ba
vaibhav <vabhizz007@gmail.com>
2017-07-15 14:26:34 +0530

    db files created and models created

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+	def index
+		@content = 'this is the sample test for post controller';
+	end
+
+	def	new
+
+	end
+	
+	def create
+	
+	end 	
+	
+	def edit
+	
+	end
+	
+	def update
+	
+	end
+	
+	def show
+	
+	end
+	
+	def destroy
+	
+	end
+	
+		
+end
